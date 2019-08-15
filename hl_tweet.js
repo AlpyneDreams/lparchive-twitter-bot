45800fa76e39d8c5c2e45434b2c59f3adda56813
DankParrot <dankparrot1@gmail.com>
2019-08-15 00:26:35 -0700

    Support multiple images

--- hl_tweet.js
+++ hl_tweet.js
@@ -12,21 +12,35 @@ console.log(`PAGE ${index.page} IMAGE ${index.img}`)
 
 let img = images[index.page - 1][index.img - 1]
 
-let text = img.text.trim()
-let matches = text.match(/:\w+:/gi)
-for (let m of matches) {
-	let name = m.slice(1, -1)
-	if (name in emotes) {
-		// HANDLE EMOTES HERE
+
+if (img.text) {
+	let text = img.text.trim()
+	let matches = text.match(/:\w+:/gi)
+
+	for (let m of matches) {
+		let name = m.slice(1, -1)
+		if (name in emotes) {
+			// HANDLE EMOTES HERE
 		
-		console.log(m)
-		text = text.replace(m, emotes[name])
+			console.log(m)
+			text = text.replace(m, emotes[name])
+		}
 	}
+	console.log(text)
 }
 
-
-console.log(text)
-console.log(img.src)
+if (img.src) {
+	
+	if (Array.isArray(img.src)) {
+		for (let i of img.src) {
+			console.log(i)
+		}
+	} else {
+		// just one image
+		console.log(img.src)
+	}
+	
+}
 
 // TWEET HERE
 
