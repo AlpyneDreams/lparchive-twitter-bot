2186fecec6c2c39d2870a6c79599cb75ea2c77a6
DankParrot <dankparrot1@gmail.com>
2019-08-15 00:19:06 -0700

    Update emote system

--- hl_tweet.js
+++ hl_tweet.js
@@ -1,14 +1,8 @@
 const images = require('./hl.json')
 
-const emotes = {
-	gonk: "https://lparchive.org/Half-Life-2/Update%2044/emot-gonk.gif",
-	siren: "https://lparchive.org/Half-Life-2/Update%2044/emot-siren.gif",
-	argh: "https://lparchive.org/Half-Life-2/Update%2036/emot-argh.gif",
-	iceburn: "https://lparchive.org/Half-Life-2/Update%2015/emot-iceburn.gif",
-	frown: "https://lparchive.org/Half-Life-2/Update%2013/frown.gif"
-}
+const emotes = require('./hl_emotes.json')
 
-let index = require('./hl_tweet_num.json')
+let index = require('./hl_state.json')
 
 // END OF CONFIGURATION //
 
@@ -17,20 +11,45 @@ const fs = require('fs')
 console.log(`PAGE ${index.page} IMAGE ${index.img}`)
 
 let img = images[index.page - 1][index.img - 1]
-console.log(img.text.trim())
+
+let text = img.text.trim()
+let matches = text.match(/:\w+:/gi)
+for (let m of matches) {
+	let name = m.slice(1, -1)
+	if (name in emotes) {
+		// HANDLE EMOTES HERE
+		
+		console.log(m)
+		text = text.replace(m, emotes[name])
+	}
+}
+
+
+console.log(text)
 console.log(img.src)
 
 // TWEET HERE
 
-if ( images[index.page - 1][index.img] ) {
-	index.img++;
-} else {
-	index.page++;
-	index.img = 1;
+if ( !index.pause ) {
+
+	if ( images[index.page - 1][index.img] ) {
+		index.img++;
+	} else {
+		index.page++;
+		index.img = 1;
+	}
+	
+	fs.writeFileSync('./hl_tweet_num.json', JSON.stringify(index))
 }
 
-fs.writeFileSync('./hl_tweet_num.json', JSON.stringify(index))
-
+/*
+hl_state.json
+{
+	page: page number, starts at 1
+	img: image number, starts at 1
+	pause: (bool) whether to increment page and image number
+}
+*/
 /* 
 hl.json contains an array of arrays, one for each page
 each array contains every image object on that page
@@ -41,20 +60,3 @@ image format:
 }
 
 */
-
-
-
-/*let part = 1
-for (let page of images) {
-	console.log(`PART ${part}: ${page.length} IMAGES`)
-	for (let img of page) {
-		if (img.text && img.text.length > 0) continue
-		if (img.text) {
-			console.log(String(img.text).trim())
-		}
-		if (img.src) {
-			console.log(img.src)
-		}
-	}
-	part++;
-}*/
