f25f6802ecdf749d751c2a4f638830cb98634044
DankParrot <dankparrot1@gmail.com>
2020-01-21 12:05:40 -0800

    Improve text/image extraction

--- lp_bookmark.js
+++ lp_bookmark.js
@@ -1,7 +1,17 @@
 javascript: (function(){
 let out = [];
+let lastimg = document.getElementById("content").querySelector("h3");
 for (let img of document.getElementById("content").getElementsByTagName("img")) {
-	out.push({src:img.src,text:img.previousSibling.previousSibling.previousSibling.textContent});
+	let txt = "";
+	if (lastimg) { 
+		let node = lastimg.nextSibling;
+		while (node.tagName != "IMG") {
+			txt += node.textContent.replace(/^[\n\t]+/gi, '');
+			node = node.nextSibling;
+		}
+	}
+	out.push({ src: img.src, text: txt });
+	lastimg = img;
 }
 navigator.clipboard.writeText(JSON.stringify(out, null, "\t") + ",\n").catch(console.error);
 })();
